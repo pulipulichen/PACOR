78f8c8d5bbc3d66727c5f2f7a1bab6dee9d82fd3
Pulipuli Chen <pulipuli.chen@gmail.com>
2019-01-08 00:10:58 +0800

    0108 why I cannot type Chinese?

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 
-var tools = require('./tools');
+var tools = require('./tools')
 
 app.get('/', function (req, res) {
-  res.send('Hello World!aaa');
-});
+  res.send('Hello World!')
+})
+
+console.log(111 * 2 + 3)
 
 app.listen(3001, function () {
-  console.log('Example app listening on port 3001aaa!');
-  
+  console.log('Example app listening on port 3001!')
+
   tools.foo()
-});
+})
