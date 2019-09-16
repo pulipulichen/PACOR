f069f85fb7aebc944dd04777c3f487444a0fdd07
Pulipuli Chen <blog@pulipuli.info>
2019-09-16 21:09:24 +0800

    0916 #3 來換引擎

--- api/models/Product.js
+++
@@ -1,9 +0,0 @@
-// api/models/Product.js
-module.exports = {
-  attributes: {
-    nameOnMenu: { type: 'string', required: true },
-    price: { type: 'string', required: true },
-    percentRealMeat: { type: 'number', defaultsTo: 20, columnType: 'FLOAT' },
-    numCalories: { type: 'number' },
-  },
-};
