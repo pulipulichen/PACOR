697dc00266e13a22203132fb331de1edfe417f29
Pulipuli Chen <blog@pulipuli.info>
2019-09-16 21:54:01 +0800

    0916 #3 練習中，好像要更新很多檔案...

---
+++ feathers-chat/src/services/messages/messages.service.js
@@ -0,0 +1,20 @@
+// Initializes the `messages` service on path `/messages`
+const { Messages } = require('./messages.class');
+const hooks = require('./messages.hooks');
+
+module.exports = function (app) {
+  
+  const paginate = app.get('paginate');
+
+  const options = {
+    paginate
+  };
+
+  // Initialize our service with any options it requires
+  app.use('/messages', new Messages(options, app));
+
+  // Get our initialized service so that we can register hooks
+  const service = app.service('messages');
+
+  service.hooks(hooks);
+};
