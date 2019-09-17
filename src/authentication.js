3ac19054ca253472a00973928c0713a0325a4fb5
Pulipuli Chen <blog@pulipuli.info>
2019-09-17 16:49:30 +0800

    0917 #3 仍在授權中

--- src/authentication.js
+++ src/authentication.js
@@ -2,12 +2,26 @@ const { AuthenticationService, JWTStrategy } = require('@feathersjs/authenticati
 const { LocalStrategy } = require('@feathersjs/authentication-local');
 const { expressOauth } = require('@feathersjs/authentication-oauth');
 
+class GitHubStrategy extends OAuthStrategy {
+  async getEntityData(profile) {
+    const baseData = await super.getEntityData(profile);
+
+    console.log(baseData)
+
+    return {
+      //...baseData,
+      email: profile.email
+    };
+  }
+}
+
 module.exports = app => {
   const authentication = new AuthenticationService(app);
 
   authentication.register('jwt', new JWTStrategy());
   authentication.register('local', new LocalStrategy());
-
+  authentication.register('github', new GitHubStrategy());
+  
   app.use('/authentication', authentication);
   app.configure(expressOauth());
 };
