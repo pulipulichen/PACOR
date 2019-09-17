59c166d298b4350d4fcce95212f08fcf596cb44d
Pulipuli Chen <blog@pulipuli.info>
2019-09-17 22:54:39 +0800

    0917 #3 建立app完成

--- .document/20190917-rebuild-pgsql/src/services/users/users.service.js
+++ .document/20190917-rebuild-pgsql/src/services/users/users.service.js
@@ -1,30 +1,22 @@
-// This is the database adapter service class
-const { Service } = require('feathers-nedb');
-// We need this to create the MD5 hash
-const crypto = require('crypto');
+// Initializes the `users` service on path `/users`
+const { Users } = require('./users.class');
+const createModel = require('../../models/users.model');
+const hooks = require('./users.hooks');
 
-// The Gravatar image service
-const gravatarUrl = 'https://s.gravatar.com/avatar';
-// The size query. Our chat needs 60px images
-const query = 's=60';
+module.exports = function (app) {
+  const Model = createModel(app);
+  const paginate = app.get('paginate');
 
-exports.Users = class Users extends Service {
-  create (data, params) {
-    // This is the information we want from the user signup data
-    const { email, password, githubId } = data;
-    // Gravatar uses MD5 hashes from an email address (all lowercase) to get the image
-    const hash = crypto.createHash('md5').update(email.toLowerCase()).digest('hex');
-    // The full avatar URL
-    const avatar = `${gravatarUrl}/${hash}?${query}`;
-    // The complete user
-    const userData = {
-      email,
-      password,
-      githubId,
-      avatar
-    };
+  const options = {
+    Model,
+    paginate
+  };
 
-    // Call the original `create` method with existing `params` and new data
-    return super.create(userData, params);
-  }  
+  // Initialize our service with any options it requires
+  app.use('/users', new Users(options, app));
+
+  // Get our initialized service so that we can register hooks
+  const service = app.service('users');
+
+  service.hooks(hooks);
 };
