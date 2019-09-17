2cab94482ba38ea68f06336a7048eee81b14a661
Pulipuli Chen <blog@pulipuli.info>
2019-09-17 23:00:17 +0800

    0917 #3 重建pgsql app: 建立auth完成，到這邊為止正常運作

--- .document/20190917-rebuild-pgsql/src/authentication.js
+++ .document/20190917-rebuild-pgsql/src/authentication.js
@@ -1,13 +1,25 @@
 const { AuthenticationService, JWTStrategy } = require('@feathersjs/authentication');
 const { LocalStrategy } = require('@feathersjs/authentication-local');
-const { expressOauth } = require('@feathersjs/authentication-oauth');
+const { expressOauth, OAuthStrategy } = require('@feathersjs/authentication-oauth');
+
+class GitHubStrategy extends OAuthStrategy {
+  async getEntityData(profile) {
+    const baseData = await super.getEntityData(profile);
+
+    return {
+      ...baseData,
+      email: profile.email
+    };
+  }
+}
 
 module.exports = app => {
-  const authentication = new AuthenticationService(app);
+  const authService = new AuthenticationService(app);
 
-  authentication.register('jwt', new JWTStrategy());
-  authentication.register('local', new LocalStrategy());
+  authService.register('jwt', new JWTStrategy());
+  authService.register('local', new LocalStrategy());
+  authService.register('github', new GitHubStrategy());
 
-  app.use('/authentication', authentication);
+  app.use('/authentication', authService);
   app.configure(expressOauth());
 };
