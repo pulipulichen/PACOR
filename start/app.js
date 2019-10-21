3d103fb6a10562ef90d333787cad95429440dd99
Pulipuli Chen <blog@pulipuli.info>
2019-10-22 01:49:25 +0800

    1019 #40 基本框架好像做好了，我們來試試看好了

--- start/app.js
+++ start/app.js
@@ -35,7 +35,9 @@ const providers = [
 |
 */
 const aceProviders = [
-  '@adonisjs/lucid/providers/MigrationsProvider'
+  '@adonisjs/lucid/providers/MigrationsProvider',
+  '@adonisjs/vow/providers/VowProvider',
+  '@adonisjs/vow-browser/providers/VowBrowserProvider'
 ]
 
 /*
---
+++ test/functional/user.spec.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const { test } = use('Test/Suite')('User')
+
+test('make sure 6 + 6 is 12', async ({ assert }) => {
+  assert.equal(6 + 5, 12)
+})
