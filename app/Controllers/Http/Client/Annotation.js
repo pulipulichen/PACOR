0e9748971ef4ae7581df1e91d651308589c028ad
Pulipuli Chen <blog@pulipuli.info>
2019-10-23 16:00:27 +0800

    1023 close #41 很快就加進去了 close #39 昨天好像做完了 #38 我想要研究一下session的部分，好像這個有點問題

--- app/Controllers/Http/Client/Annotation.js
+++ app/Controllers/Http/Client/Annotation.js
@@ -6,6 +6,9 @@ const ReadingActivityLog = use ('App/Models/ReadingActivityLog')
 const AnnotationAnchorTextModel = use('App/Models/AnnotationAnchorText')
 const AnnotationModel = use('App/Models/Annotation')
 
+const Cache = use('Cache')
+const Config = use('Config')
+
 class Annotation extends WebpageUserBaseController {
   constructor () {
     super('Annotation')
@@ -17,6 +20,31 @@ class Annotation extends WebpageUserBaseController {
     
     let instance = await AnnotationModel.create(webpage, user, data)
     return instance.id
+  }
+  
+  async indexOthers ({ request, webpage, user }) {
+    let condition = request.all()
+    await ReadingActivityLog.log(webpage, user, this.modelName + '.indexOthers', condition)
+    
+    let cacheKey = `${this.modelName}.indexOthers.${webpage.id}.${user.id}.${JSON.stringify(condition)}`
+    return await Cache.get(cacheKey, async () => {
+      let others = await user.getOtherUsersInGroup(webpage)
+
+      let query = this.model
+              .query()
+              .where('webpage_id', webpage.primaryKeyValue)
+              .whereIn('user_id', others)
+              .where('private', false)
+              .where('deleted', false)
+
+      if (typeof(condition) === 'object') {
+        query.where(condition)
+      }
+
+      let output = await query.fetch()
+      await Cache.put(cacheKey, output, Config.get('view.indexCacheMinute'))
+      return output
+    })
   }
 }
 
