1b293451ba3243293a53a02bb1eb4aa4182a4464
Pulipuli Chen <blog@pulipuli.info>
2020-01-23 23:10:05 +0800

    0123 #319 不要灰心，繼續

---
+++ webpack-app/client/Questionnaire/components/Instruction/PostRecallInstruction/PostRecallInstruction.vue
@@ -0,0 +1,4 @@
+<template src="./PostRecallInstruction.html"></template>
+<script src="./PostRecallInstruction.js"></script>
+<style src="./PostRecallInstruction.less" lang="less" scoped></style>
+<i18n src="./PostRecallInstruction.yaml" lang="yaml"></i18n>
