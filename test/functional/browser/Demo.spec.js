ce8e4d92b59103048655aee5a6f033b27af6ea3a
Pulipuli Chen <blog@pulipuli.info>
2020-02-21 16:15:28 +0800

    0221 #334 似乎不是我的問題... #310

--- test/functional/browser/Demo.spec.js
+++ test/functional/browser/Demo.spec.js
@@ -3,10 +3,12 @@
 const { test, trait } = use('Test/Suite')('Hello World')
 
 trait('Test/Browser', {
-  headless: false
+  //headless: false
 })
 
 test('Visit home page', async ({ browser }) => {
+  console.log('AAA 1')
   const page = await browser.visit('http://blog.pulipuli.info/')
+  console.log('AAA 2')
   await page.assertHas('布丁布丁吃什麼')
 }).timeout(0)
