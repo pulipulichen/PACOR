186e7516b684b69ff86a51aaed04ab1c07897db6
Pulipuli Chen <blog@pulipuli.info>
2020-03-02 14:56:01 +0800

    0302 #384 然後是文章標題的部分

--- public/spa/client-components/Loading.js
+++ public/spa/client-components/Loading.js
@@ -1977,7 +1977,31 @@ let LoginMessage = {
       return this.status.readingConfig.readingProgresses
     },
     totalTime () {
-      return 10
+      let time = 0
+      let modules = this.status.readingConfig.readingProgressModules
+      let hasAddedReading = false
+      
+      this.steps.forEach(step => {
+        let module = modules[step]
+        if (!module) {
+          return false
+        }
+        
+        if (['IndividualReading', 'CollaborativeReading'].indexOf(step) > -1) {
+          if (hasAddedReading === true) {
+            return false
+          }
+          //console.log('reading', modules['reading'].totalLimitMinutes)
+          time = time + modules['reading'].totalLimitMinutes
+          hasAddedReading = true
+          return false
+        }
+        
+        //console.log('step', step, module.limitMinutes)
+        time = time + module.limitMinutes
+      })
+      
+      return time
     },
     computedTotalTimeMessage () {
       return this.$t('It takes about <span class="highlight">{0} minutes</span> in total.', [this.totalTime])
